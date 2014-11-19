80cdd37b97ba2f58732db623e6e58f67eb7d740f
dahuan <liuyujiahuan@163.com>
2014-11-19 15:34:13 +0800

    Initial Commit

---
+++ Invitation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Invitation
+//
+//  Created by 大 欢 on 14/11/19.
+//  Copyright (c) 2014年 DongShenNianYi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
